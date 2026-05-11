a12db9baa1ad075af7a5fcc229df27537d2fae09
romaska877 <iliciuctiberius@gmail.com>
2026-05-11 12:44:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const contactButton = document.getElementById("contactButton");
+const contactMessage = document.getElementById("contactMessage");
+
+contactButton.addEventListener("click", function () {
+  contactMessage.textContent =
+    "Thanks for checking my work. I am currently available for small beginner website projects.";
+});
